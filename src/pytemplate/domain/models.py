28761144d55db071e4cf5427a9b87005e5a2b6c2
aliyev24 <x.rodriguez@mail.ru>
2024-08-17 12:26:34 +0400

    fix: updated Operand class with @dataclass

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -1,12 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
 class Operand:
-
-    def __init__(self, first_operand:int, second_operand:int) -> None:
-        """
-
-        :param first_operand:
-        :param second_operand:
-        """
-
-        self.first_operand = first_operand
-        self.second_operand = second_operand
+    first_operand: int
+    second_operand: int
 
